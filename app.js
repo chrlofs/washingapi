1b63fc154d15e0086795c1c5d80e1a011e323ec4
Christoffer Lofsberg <chris.lofsberg@gmail.com>
2016-02-12 23:47:37 +0100

    Add caching

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 var express = require('express')
 var request = require('request')
 var cheerio = require('cheerio')
+var apicache = require('apicache').options({debug:true}).middleware
 var app = express()
 
 var router = express.Router()
 
 var url = 'http://129.241.124.32:80/LaundryState'
 
-router.get('/washingapi', function (req, res) {
+router.get('/washingapi', apicache('5 minutes'), function (req, res) {
   console.log("Got a new request")
 
   var user = req.param('user')
   var password = req.param('pass')
 
-  if (user === '' || password === '') {
-    res.send("Username or password not entered")
+  if (user == '' || password == '') {
+    res.json({error: "Username or password not entered"})
   }
 
   request.get(url, {
@@ -44,9 +45,10 @@ router.get('/washingapi', function (req, res) {
             }
           }
         })
-        res.send(data)
+        res.header("Content-Type", "application/json")
+        res.json(data)
       } else {
-        res.send("Something went wrong")
+        res.json({error: "Something went wrong"})
       }
   })
 })
